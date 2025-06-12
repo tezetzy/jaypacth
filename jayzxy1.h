ce7ebb4c302be7f8a7e1efdfd6cc921af940b66e
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 06:04:13 +0700

    Update jayzxy1.h

--- jayzxy1.h
+++ jayzxy1.h
@@ -1,4 +1,4 @@
-#include <inc\CDraw1.h>
-#include <inc\CPlayerInfo1.h>
-#include <inc\CTimer1.h>
-#include <inc\CVector1.h>
+#include <inc/CDraw1.h>
+#include <inc/CPlayerInfo1.h>
+#include <inc/CTimer1.h>
+#include <inc/CVector1.h>
