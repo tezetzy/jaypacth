61d760a5a3871971e0becfe1d995ed44413aee28
tezetzy <firlanrizqulloh@gmail.com>
2025-06-13 03:06:31 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,338 @@
+#include <mod/amlmod.h>
+#include <mod/logger.h>
+#include <mod/config.h>
+#include <dlfcn.h>
+
+#include "GTASA_STRUCTS.h"
+
+MYMODCFG(net.jaysiii.aml, Logika, 1.0, jaysiii)
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Saves     ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+uintptr_t pGTASA;
+void* hGTASA;
+static constexpr float fMagic = 50.0f / 30.0f;
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Vars      ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+float *ms_fTimeStep;
+CPlayerInfo* WorldPlayers;
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Funcs     ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+void Redirect(uintptr_t addr, uintptr_t to)
+{
+    if(!addr) return;
+    if(addr & 1)
+    {
+        addr &= ~1;
+        if (addr & 2)
+        {
+            aml->PlaceNOP(addr, 1);
+            addr += 2;
+        }
+        uint32_t hook[2];
+        hook[0] = 0xF000F8DF;
+        hook[1] = to;
+        aml->Write(addr, (uintptr_t)hook, sizeof(hook));
+    }
+    else
+    {
+        uint32_t hook[2];
+        hook[0] = 0xE51FF004;
+        hook[1] = to;
+        aml->Write(addr, (uintptr_t)hook, sizeof(hook));
+    }
+}
+void (*_rwOpenGLSetRenderState)(RwRenderState, int);
+void (*_rwOpenGLGetRenderState)(RwRenderState, void*);
+void (*ClearPedWeapons)(CPed*);
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Hooks     ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+extern "C" void adadad(void)
+{
+    asm("VMOV.F32 S0, #0.5");
+}
+DECL_HOOKv(EntityRender, CEntity* ent)
+{
+    if(ent->m_nType == ENTITY_TYPE_VEHICLE)
+    {
+        EntityRender(ent);
+        return;
+    }
+
+    //RwRenderStateSet(rwRENDERSTATECULLMODE, (void*)rwCULLMODECULLNONE);
+    EntityRender(ent);
+    //RwRenderStateSet(rwRENDERSTATECULLMODE, (void*)rwCULLMODECULLBACK);
+}
+DECL_HOOKv(ControlGunMove, void* self, CVector2D* vec2D) // AimingRifleWalkFix
+{
+    float save = *ms_fTimeStep; *ms_fTimeStep = fMagic;
+    ControlGunMove(self, vec2D);
+    *ms_fTimeStep = save;
+}
+DECL_HOOKv(ProcessSwimmingResistance, CTaskSimpleSwim* task, CPed* ped)
+{
+    float fSubmergeZ = -1.0f;
+    CVector vecPedMoveSpeed{};
+
+    switch (task->m_nSwimState)
+    {
+        case SWIM_TREAD:
+        case SWIM_SPRINT:
+        case SWIM_SPRINTING: {
+            float fAnimBlendSum = 0.0f;
+            float fAnimBlendDifference = 1.0f;
+
+            CAnimBlendAssociation* animSwimBreast = RpAnimBlendClumpGetAssociationU(ped->m_pRwClump, ANIM_ID_SWIM_BREAST);
+            if (animSwimBreast) {
+                fAnimBlendSum = 0.4f * animSwimBreast->m_fBlendAmount;
+                fAnimBlendDifference = 1.0f - animSwimBreast->m_fBlendAmount;
+            }
+
+            CAnimBlendAssociation* animSwimCrawl = RpAnimBlendClumpGetAssociationU(ped->m_pRwClump, ANIM_ID_SWIM_CRAWL);
+            if (animSwimCrawl) {
+                fAnimBlendSum += 0.2f * animSwimCrawl->m_fBlendAmount;
+                fAnimBlendDifference -= animSwimCrawl->m_fBlendAmount;
+            }
+            if (fAnimBlendDifference < 0.0f) {
+                fAnimBlendDifference = 0.0f;
+            }
+
+            fSubmergeZ = fAnimBlendDifference * 0.55f + fAnimBlendSum;
+
+            vecPedMoveSpeed =  ped->m_vecAnimMovingShiftLocal.x * ped->GetRight();
+            vecPedMoveSpeed += ped->m_vecAnimMovingShiftLocal.y * ped->GetForward();
+            break;
+        }
+        case SWIM_DIVE_UNDERWATER: {
+            vecPedMoveSpeed =  ped->m_vecAnimMovingShiftLocal.x * ped->GetRight();
+            vecPedMoveSpeed += ped->m_vecAnimMovingShiftLocal.y * ped->GetForward();
+
+            auto animSwimDiveUnder = RpAnimBlendClumpGetAssociationU(ped->m_pRwClump, ANIM_ID_SWIM_DIVE_UNDER);
+            if (animSwimDiveUnder) {
+                vecPedMoveSpeed.z = animSwimDiveUnder->m_fCurrentTime / animSwimDiveUnder->m_pAnimBlendHierarchy->m_fTotalTime * 
+                #ifndef SWIMSPEED_FIX
+                    -0.1f;
+                #else
+                    (-0.1f * GetTimeStepMagic());
+                #endif
+            }
+            break;
+        }
+        case SWIM_UNDERWATER_SPRINTING: {
+            vecPedMoveSpeed   =  ped->m_vecAnimMovingShiftLocal.x * ped->GetRight();
+            vecPedMoveSpeed   += cosf(task->m_fRotationX) * ped->m_vecAnimMovingShiftLocal.y * ped->GetForward();
+            vecPedMoveSpeed.z += (sinf(task->m_fRotationX) * ped->m_vecAnimMovingShiftLocal.y + 0.01f)
+            #ifdef SWIMSPEED_FIX
+                / GetTimeStepMagic()
+            #endif
+            ;
+            break;
+        }
+        case SWIM_BACK_TO_SURFACE: {
+            auto animClimb = RpAnimBlendClumpGetAssociationU(ped->m_pRwClump, ANIM_ID_CLIMB_JUMP);
+            if (!animClimb)
+                animClimb = RpAnimBlendClumpGetAssociationU(ped->m_pRwClump, ANIM_ID_SWIM_JUMPOUT);
+
+            if (animClimb) {
+                if (animClimb->m_pAnimBlendHierarchy->m_fTotalTime > animClimb->m_fCurrentTime &&
+                    (animClimb->m_fBlendAmount >= 1.0f || animClimb->m_fBlendDelta > 0.0f)
+                ) {
+                    float fMoveForceZ = GetTimeStep() * ped->m_fMass * 0.3f * 0.008f;
+                    ApplyMoveForce(ped, 0.0f, 0.0f, fMoveForceZ);
+                }
+            }
+            return;
+        }
+        default: {
+            return;
+        }
+    }
+
+    vecPedMoveSpeed *= (1.0f - fTheTimeStep)
+    #ifdef SWIMSPEED_FIX
+        * GetTimeStepInvMagic()
+    #endif
+    ;
+    ped->m_vecMoveSpeed *= fTheTimeStep;
+    #ifdef SWIMSPEED_FIX
+        if(ped->IsPlayer()) vecPedMoveSpeed *= 1.25f;
+    #endif
+    ped->m_vecMoveSpeed += vecPedMoveSpeed;
+
+    auto& pedPos = ped->GetPosition();
+    bool bUpdateRotationX = true;
+    CVector vecCheckWaterLevelPos = GetTimeStep() * ped->m_vecMoveSpeed + pedPos;
+    float fWaterLevel = 0.0f;
+    if (!GetWaterLevel(vecCheckWaterLevelPos, fWaterLevel, true, NULL)) {
+        fSubmergeZ = -1.0f;
+        bUpdateRotationX = false;
+    } else {
+        if (task->m_nSwimState != SWIM_UNDERWATER_SPRINTING || task->m_fStateChanger < 0.0f) {
+            bUpdateRotationX = false;
+        } else {
+            if (pedPos.z + 0.65f > fWaterLevel && task->m_fRotationX > 0.7854f) {
+                task->m_nSwimState = SWIM_TREAD;
+                task->m_fStateChanger = 0.0f;
+                bUpdateRotationX = false;
+            }
+        }
+    }
+
+    if (bUpdateRotationX) {
+        if (task->m_fRotationX >= 0.0f) {
+            if (pedPos.z + 0.65f <= fWaterLevel) {
+                if (task->m_fStateChanger <= 0.001f)
+                    task->m_fStateChanger = 0.0f;
+                else
+                    task->m_fStateChanger *= 0.95f;
+            } else {
+                float fMinimumSpeed = 0.05f * 0.5f;
+                if (task->m_fStateChanger > fMinimumSpeed) {
+                    task->m_fStateChanger *= 0.95f;
+                }
+                if (task->m_fStateChanger < fMinimumSpeed) {
+                    task->m_fStateChanger += GetTimeStepInSeconds() / 10.0f;
+                    task->m_fStateChanger = std::min(fMinimumSpeed, task->m_fStateChanger);
+                }
+                task->m_fRotationX += GetTimeStep() * task->m_fStateChanger;
+                fSubmergeZ = (0.55f - 0.2f) * (task->m_fRotationX * 4.0f / PI) * 0.75f + 0.2f;
+            }
+        } else {
+            if (pedPos.z - sin(task->m_fRotationX) + 0.65f <= fWaterLevel) {
+                if (task->m_fStateChanger > 0.001f)
+                    task->m_fStateChanger *= 0.95f;
+                else
+                    task->m_fStateChanger = 0.0f;
+            } else {
+                task->m_fStateChanger += GetTimeStepInSeconds() / 10.0f;
+                task->m_fStateChanger = std::min(task->m_fStateChanger, 0.05f);
+            }
+            task->m_fRotationX += GetTimeStep() * task->m_fStateChanger;
+        }
+    }
+float fWideScreenWidthScale, fWideScreenHeightScale;
+DECL_HOOKv(DrawCrosshair)
+{
+    static constexpr float XSVal = 1024.0f / 1920.0f; // prev. 0.530, now it's 0.533333..3
+    static constexpr float YSVal = 768.0f / 1920.0f; // unchanged :p
+
+    CPlayerPed* player = WorldPlayers[0].m_pPed;
+    if(player->m_Weapons[player->m_byteCurrentWeaponSlot].m_nType == WEAPON_COUNTRYRIFLE)
+    {
+        // Weirdo logic but ok
+        float save1 = *m_f3rdPersonCHairMultX; *m_f3rdPersonCHairMultX = 0.530f - 0.84f * ar43 * 0.01115f; // 0.01125f;
+        float save2 = *m_f3rdPersonCHairMultY; *m_f3rdPersonCHairMultY = 0.400f + 0.84f * ar43 * 0.038f; // 0.03600f;
+        DrawCrosshair();
+        *m_f3rdPersonCHairMultX = save1; *m_f3rdPersonCHairMultY = save2;
+        return;
+    }
+
+    float save1 = *m_f3rdPersonCHairMultX; *m_f3rdPersonCHairMultX = 0.530f - fAspectCorrection * 0.01115f; // 0.01125f;
+    float save2 = *m_f3rdPersonCHairMultY; *m_f3rdPersonCHairMultY = 0.400f + fAspectCorrection * 0.038f; // 0.03600f;
+    DrawCrosshair();
+    *m_f3rdPersonCHairMultX = save1; *m_f3rdPersonCHairMultY = save2;
+}
+
+DECL_HOOKv(CalculateAspectRatio_CrosshairFix)
+{
+    CalculateAspectRatio_CrosshairFix();
+
+    fWideScreenWidthScale = 640.0f / (*ms_fAspectRatio * 448.0f);
+    fWideScreenHeightScale = 448.0 / 448.0f;
+}
+uintptr_t DrawMoney_BackTo;
+extern "C" const char* DrawMoney_Patch(int isPositive)
+{
+    static const char* positiveT = "$%08d";
+    static const char* negativeT = "-$%07d";
+    
+    return isPositive ? positiveT : negativeT;
+}
+__attribute__((optnone)) __attribute__((naked)) void DrawMoney_Inject(void)
+{
+    asm volatile(
+        "LDR R5, [R0]\n"
+        "SMLABB R0, R1, R11, R5\n"
+        "PUSH {R0-R11}\n"
+        "LDR R0, [SP, #8]\n"
+        "BL DrawMoney_Patch\n"
+        "STR R0, [SP, #4]\n");
+    asm volatile(
+        "MOV R12, %0\n"
+        "POP {R0-R11}\n"
+        "BX R12\n"
+    :: "r" (DrawMoney_BackTo));
+}
+DECL_HOOK(ScriptHandle, GenerateNewPickup, float x, float y, float z, int16_t modelId, ePickupType pickupType, int ammo, int16_t moneyPerDay, bool isEmpty, const char* msg)
+{
+    if(modelId == 1277 && x == 1263.05f && y == -773.67f && z == 1091.39f)
+    {
+        return GenerateNewPickup(1291.2f, -798.0f, 1089.39f, modelId, pickupType, ammo, moneyPerDay, isEmpty, msg);
+    }
+    return GenerateNewPickup(x, y, z, modelId, pickupType, ammo, moneyPerDay, isEmpty, msg);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///////////////////////////////     Funcs     ///////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+extern "C" void OnModLoad()
+{
+    pGTASA = aml->GetLib("libGTASA.so");
+    hGTASA = dlopen("libGTASA.so", RTLD_LAZY);
+
+    // Functions Start //
+    SET_TO(_rwOpenGLSetRenderState, aml->GetSym(hGTASA, "_Z23_rwOpenGLSetRenderState13RwRenderStatePv"));
+    SET_TO(_rwOpenGLGetRenderState, aml->GetSym(hGTASA, "_Z23_rwOpenGLGetRenderState13RwRenderStatePv"));
+    SET_TO(ClearPedWeapons, aml->GetSym(hGTASA, "_ZN4CPed12ClearWeaponsEv"));
+    // Functions End   //
+    
+    // Variables Start //
+    SET_TO(ms_fTimeStep, aml->GetSym(hGTASA, "_ZN6CTimer12ms_fTimeStepE"));
+    SET_TO(WorldPlayers, aml->GetSym(hGTASA, "_ZN6CWorld7PlayersE"));
+    // Variables End   //
+
+    // Fix walking while rifle-aiming
+    if(cfg->Bind("FixAimingWalkRifle", true, "Gameplay")->GetBool())
+    {
+        HOOKPLT(ControlGunMove, pGTASA + 0x66F9D0);
+    }
+
+    // Fix slow swimming speed
+    if(cfg->Bind("SwimmingSpeedFix", true, "Gameplay")->GetBool())
+    {
+        HOOKPLT(ProcessSwimmingResistance, pGTASA + 0x66E584);
+    }
+    if(cfg->GetBool("HighFPSAimingWalkingFix", true, "Gameplay"))
+    {
+        aml->Unprot(pGTASA + 0x4DD9E8, sizeof(float));
+        *(float*)(pGTASA + 0x4DD9E8) = 0.015f;
+        //SET_TO(float_4DD9E8, pGTASA + 0x4DD9E8);
+        //HOOK(TaskSimpleUseGunSetMoveAnim, aml->GetSym(hGTASA, "_ZN17CTaskSimpleUseGun11SetMoveAnimEP4CPed"));
+    }
+    if(cfg->GetBool("FixCrosshair", true, "Visual"))
+    {
+        HOOKPLT(DrawCrosshair, pGTASA + 0x672880);
+        //HOOK(CalculateAspectRatio_CrosshairFix, aml->GetSym(hGTASA, "_ZN5CDraw20CalculateAspectRatioEv"));
+    }
+    if(cfg->GetBool("PCStyledMoney", false, "Visual"))
+    {
+        DrawMoney_BackTo = pGTASA + 0x2BD260 + 0x1;
+        aml->Redirect(pGTASA + 0x2BD258 + 0x1, (uintptr_t)DrawMoney_Inject);
+    }
+    if(cfg->GetBool("AllowCrouchWith1HP", true, "Gameplay"))
+    {
+        aml->Write(pGTASA + 0x54316C, "\xB4", 1);
+    }
+    if(cfg->GetBool("FixDrunkCameraHighFPS", true, "Visual"))
+    {
+        HOOKPLT(CameraProcess_HighFPS, pGTASA + 0x6717BC);
+    }
+
+}
